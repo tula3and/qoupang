3df4251cfec6377a35cba2302a4ca882b76c239f
Dayeong Kang <tula3and@gmail.com>
2023-06-27 07:21:15 +0000

    Fix bugs in hash8 function fixed #1 and #2

--- qrng/qrng.py
+++ qrng/qrng.py
@@ -15,7 +15,7 @@ circuit.h(q)
 for i in range(47):
     circuit.cx(q[i], q[47])
 
-circuit.measure_all()
+circuit.measure(q,c)
 
 import string
 table = string.ascii_uppercase + string.ascii_lowercase + string.digits
@@ -24,11 +24,11 @@ def hash8():
     hash_result = ''
     result = execute(circuit, backend, shots=1).result()
     count = result.get_counts(circuit)
-    bits = max(count, key=lambda i: count[i])[:48]
+    bits = max(count, key=lambda i: count[i])
     start = 0
     end = 6
     while (end <= 48):
-        rand = int(bits[start:end], 2) % 63
+        rand = int(bits[start:end], 2) % len(table)
         start += 6
         end += 6
         hash_result += table[rand]
